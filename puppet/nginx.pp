525ee79904712e7483b807fc3508f389ba38332a
Pavel Ulyashev <pulyashev@thumbtack.net>
2013-01-21 15:22:51 +0700

    Create nginx module

--- puppet/nginx.pp
+++
@@ -1,9 +0,0 @@
-package {
-  'nginx-release':
-    ensure => installed,
-    provider => rpm,
-    source => "http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm";
-  'nginx':
-    ensure => latest,
-    require => Package['nginx-release'];
-}
