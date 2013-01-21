71f5ff938ca495ce1cbb303edb6be7af463d6d0f
Yevgeniy Medvedev <emedvedev@thumbtack.net>
2013-01-21 15:45:27 +0700

    fix nginx role

--- scripts/nginx role.sh
+++ scripts/nginx role.sh
@@ -1,6 +1,7 @@
-cd /tmp/
-#################                 nginx/1.2.6                 #################
-wget http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
-yum install -y nginx-release-centos-6-0.el6.ngx.noarch.rpm
+# Adding nginx.org repo
+yum install -y http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+
+# nginx/1.2.6
 yum install -y nginx
-###############################################################################
+chkconfig nginx on
+service nginx start
