1dd6e16803d4b227fbf6d0aa1657f60050bef747
Pavel Ulyashev <pulyashev@thumbtack.net>
2013-01-21 14:41:43 +0700

    Installer for nginx-release and than nginx

--- puppet/nginx.pp
+++ puppet/nginx.pp
@@ -1,3 +1,8 @@
-package { 'nginx':
-  ensure => latest;
-}  
+package {
+  'nginx-release':
+    ensure => installed,
+    source => http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm;
+  'nginx':
+    ensure => latest,
+    require => Package['nginx-release'];
+}
