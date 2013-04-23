917ebd948b602f44e19bcf4cffeb6b3cadc8919b
Pavel Ulyashev <p.ulyashev@gmail.com>
2013-04-23 21:50:03 +0700

    Install virtualenvwrapper from tar

--- scripts/ubuntu/python.sh
+++ scripts/ubuntu/python.sh
@@ -6,5 +6,10 @@ function install_python_tools {
 	cd -
 
 	sudo easy_install pip
-	sudo pip install virtualenv virtualenvwrapper
+	sudo pip install virtualenv
+
+	# It should be
+	# sudo pip install virtualenvwrapper
+	# but now that doesn't work
+	sudo pip install https://pypi.python.org/packages/source/v/virtualenvwrapper/virtualenvwrapper-3.7.1.tar.gz
 }
