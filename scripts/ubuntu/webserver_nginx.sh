9a7ac353982ce28ca57c5bb8bbf06a9bcd26f989
Pavel Ulyashev <p.ulyashev@gmail.com>
2013-04-23 20:48:20 +0700

    Comment out mysql installation

--- scripts/ubuntu/webserver_nginx.sh
+++ scripts/ubuntu/webserver_nginx.sh
@@ -1,5 +1,5 @@
 function install_nginx {
-	# sudo apt-get install -y nginx
-	# sudo update-rc.d nginx enable
-	# sudo service nginx start
+	sudo apt-get install -y nginx
+	sudo update-rc.d nginx enable
+	sudo service nginx start
 }
